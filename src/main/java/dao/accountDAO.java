568dea888e2b22653ae915e08fc9284f23ee13a9
TTHTech <tthoai2401.learn@gmail.com>
2023-12-09 22:43:20 +0700

    add commit

--- src/main/java/dao/accountDAO.java
+++ src/main/java/dao/accountDAO.java
@@ -5,8 +5,6 @@ import entity.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 public class accountDAO {
     Connection conn = null;
@@ -57,19 +55,68 @@ public class accountDAO {
         }
         return null;
     }
-    public void signup(String email, String password) {
-        String query = "INSERT INTO Account (email, password) VALUES (?, ?)";
+    public Account signup(String userName , String email, String password) {
+        String query = "INSERT INTO Account (userName ,email, password) VALUES (?,?, ?)";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, userName);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getUserNameByEmail(String email) {
+        String query = "SELECT userName FROM Account WHERE email = ?";
+        String userName = null;
         try {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
             ps.setString(1, email);
-            ps.setString(2, password);
-            ps.executeUpdate();
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                userName = rs.getString("userName");
+            }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        return userName;
     }
+    public static void main(String[] args) {
+        // Khởi tạo một instance của accountDAO
+        accountDAO dao = new accountDAO();
 
+        // Test với một email cụ thể
+        String testEmail = "hoaidanghocbai@gmail.com";
+        String userName = dao.getUserNameByEmail(testEmail);
 
+        if (userName != null) {
+            System.out.println("User name for email " + testEmail + " is: " + userName);
+        } else {
+            System.out.println("No user found for email " + testEmail);
+        }
+
+        // Thêm các test case khác nếu cần
+    }
 }
 
